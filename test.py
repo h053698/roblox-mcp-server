9dd0cf536102304b965e133b000f48fff8dac372
Yuchan Han <serltretu24@gmail.com>
2025-07-14 00:18:32 +0900

    Refactor authentication handling in test.py to improve key retrieval and game discovery

--- test.py
+++ test.py
@@ -1,12 +1,23 @@
+import keyring
 from fastmcp import FastMCP
+
+from credential_manager import open_app
 from rbxclient.client import RBXClient
 from keyring import get_password
 
-roblox = RBXClient(get_password("ROBLOX_MCP_SERVER", ".ROBLOXSECURITY"))
+SERVICE_ID = "ROBLOX_MCP_SERVER"
+USERNAME = ".ROBLOXSECURITY"
+
+key = keyring.get_password(SERVICE_ID, USERNAME)
+roblox = RBXClient(key)
 
 
 async def main():
-    friends = await roblox.fetch_friends(with_presence=True)
+    open_app()
+    await roblox.set_roblox_auth_key(
+        get_password("ROBLOX_MCP_SERVER", ".ROBLOXSECURITY")
+    )
+    friends = await roblox.fetch_discover_games()
     print(friends)
 
 
